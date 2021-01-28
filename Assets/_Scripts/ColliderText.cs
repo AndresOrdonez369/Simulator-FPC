2e2d4e7b304e8595a1ebdc132e098a18307ddfc2
Andres Ordóñez <oswaldordonez@unicauca.edu.co>
2021-01-27 19:10:41 -0500

    First update

---
+++ Assets/_Scripts/ColliderText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderText : MonoBehaviour
+{
+	public string popUp;
+	public string title;
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		Debug.Log('a');
+		PopUpSystem pop = GameObject.FindGameObjectWithTag("GameManager").GetComponent<PopUpSystem>();
+		pop.PopUp(popUp,title);
+	}
+	private void OnTriggerExit(Collider other)
+	{
+		PopUpSystem pop = GameObject.FindGameObjectWithTag("GameManager").GetComponent<PopUpSystem>();
+		pop.popUpBox.SetActive(false);
+	}
+}
---
+++ Assets/_Scripts/PopUpSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PopUpSystem : MonoBehaviour
+{
+
+	public GameObject popUpBox;
+	public Animator animator;
+	public TMP_Text popUpText;
+	public TMP_Text textTitle;
+	
+	public void PopUp(string text, string text2 )
+	{
+		popUpBox.SetActive(true);
+		popUpText.text = text;
+		textTitle.text = text2;
+		animator.SetTrigger("pop");
+
+	}
+
+
+}
